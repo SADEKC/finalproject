14f9456ed36fef2421e3ea5735b926af898ba82f
SADEKC <mdshadikulislamsadek74@gmail.com>
2026-05-18 17:21:12 +0600

    dashboard

--- member2/view/dashboard.php
+++ member2/view/dashboard.php
@@ -1,3 +1,21 @@
-<?php 
-echo "hello"
-?>
+
+<html>
+<body>
+<h2>Dashboard</h2>
+<a href="createArticle.php">Create Article</a>
+<br><br>
+<a href="categories.php">Manage Categories</a>
+<br><br>
+<table border="1">
+<tr>
+<th>ID</th>
+<th>Title</th>
+<th>Status</th>
+<th>Views</th>
+<th>Edit</th>
+<th>Delete</th>
+<th>Toggle</th>
+</tr>
+</table>
+</body>
+</html>
